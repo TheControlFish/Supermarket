5f9d9d03dde413a071e9551844ead1dee58a405c
TheControlFish <jacef314@gmail.com>
2016-11-10 12:48:16 -0700

    Project

---
+++ src/Customer.java
@@ -0,0 +1,15 @@
+
+public class Customer {
+	private String name;
+	private double sale;
+	public Customer(String n, double s){
+		name = n;
+		sale = s;
+	}
+	public double sale(){
+		return sale;
+	}
+	public String name(){
+		return name;
+	}
+}
